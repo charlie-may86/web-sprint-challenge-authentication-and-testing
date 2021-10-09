5c6a3d59fe3e0f70b55ae440fa6b8aa9f527e21e
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2021-10-09 09:49:58 -0500

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -80,12 +80,12 @@ router.post("/register", checkUserNameFree, (req, res, next) => {
 router.post("/login", checkUserNameExisits, (req, res) => {
   if (bcrypt.compareSync(req.body.password, req.user.password)) {
     const token = buildToken(req.user);
-    res.json({
+    res.status(200).json({
       message: `welcome, ${req.body.username}`,
       token: token,
     });
   } else {
-    res.json({ status: 401, message: "invalid credentials" });
+    res.status(401).json({ message: "invalid credentials" });
   }
 
   /*
@@ -102,11 +102,11 @@ router.post("/login", checkUserNameExisits, (req, res) => {
       the response body should have `message` and `token`:
       {
         "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
+        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8" CHECK********
       }
 
     3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
+      the response body should include a string exactly as follows: "username and password required". CHECK*******
 
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
